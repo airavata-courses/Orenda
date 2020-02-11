af98544d663a08cb65cee4e1d92f6e0b289d56e3
milan <milanchheta@gmail.com>
2020-02-11 18:48:13 -0500

    changed kafka config and data format and integrated with pyimgur  #26 #27

--- dataModelling/app.py
+++ dataModelling/app.py
@@ -8,14 +8,13 @@ conn = nexradaws.NexradAwsInterface()
 app = Flask(__name__)
 
 print('done')
-consumer = KafkaConsumer('dataModellingConsumer1')
+consumer = KafkaConsumer('dataModellingConsumerF')
 
 for message in consumer:
     if message is not None:
         dataMsg=json.loads(message.value)
         print(dataMsg,message.topic)
-        if(dataMsg['userID']):
-            produce(dataMsg,conn)
+        produce(dataMsg,conn)
 
 if __name__ == "__main__":
  app.run(host='127.0.0.0', port=8080)
--- dataModelling/services/produce.py
+++ dataModelling/services/produce.py
@@ -9,14 +9,21 @@ import base64
 import matplotlib.image as mpimg
 import json
 import pandas as pd
-
+import pyimgur
 from kafka import KafkaProducer
 producer = KafkaProducer(bootstrap_servers='localhost:9092')
+
+CLIENT_ID = "78adc2281c1c838"
+
+im = pyimgur.Imgur(CLIENT_ID)
+	
+	
+
 def produce(data,conn):
-        Year=data['data']['Year']
-        Month=data['data']['Month']
-        Day=data['data']['Day']
-        Radar=data['data']['Radar']
+        Year=data['inputData']['Year']
+        Month=data['inputData']['Month']
+        Day=data['inputData']['Day']
+        Radar=data['inputData']['Radar']
         scans = conn.get_avail_scans(Year,Month,Day,Radar) # year, month and day
         results = conn.download(scans[0], 'templocation')
 
@@ -28,23 +35,30 @@ def produce(data,conn):
                 display = pyart.graph.RadarDisplay(radar)
                 display.plot('reflectivity',0,ax=ax,title="{} {}".format(scan.radar_id,scan.scan_time))
                 display.set_limits((-150, 150), (-150, 150), ax=ax)
-        fig.canvas.draw ( )
-        w,h = fig.canvas.get_width_height()
-        buf = np.frombuffer ( fig.canvas.tostring_argb(), dtype=np.uint8 )
-        buf.shape = ( w, h,4 )
-        buf = np.roll ( buf, 3, axis = 2 )
-        w, h, d = buf.shape
-        res=Image.frombytes( "RGBA", ( w ,h ), buf.tostring( ) )
-        res=res.resize((960,720),Image.ANTIALIAS)
-        # body={
-        #         "resultPlot":buf.tolist(),
-        #         "uid":data["uid"],
-        #         "userID":data["userID"]
-        #     }
-        body='{"resultPlot":"0","uid":"8","userID":"2"}'
+        plotName=data["uid"]+'.png'
+        plt.savefig(plotName)
+        
         plt.close(fig)
-        print(json.dumps(body).encode('utf-8'))
-        producer.send('dataAnalysisConsumer1', json.dumps(body).encode('utf-8'))
+        
+        uploadImage = im.upload_image(plotName, title="Uploaded with PyImgur")
+        link=str(uploadImage.link)
+        # fig.canvas.draw ( )
+        # w,h = fig.canvas.get_width_height()
+        # buf = np.frombuffer ( fig.canvas.tostring_argb(), dtype=np.uint8 )
+        # buf.shape = ( w, h,4 )
+        # buf = np.roll ( buf, 3, axis = 2 )
+        # w, h, d = buf.shape
+        # res=Image.frombytes( "RGBA", ( w ,h ), buf.tostring( ) )
+        # res=res.resize((960,720),Image.ANTIALIAS)
+        body={  "inputData":data["inputData"],
+                "outputData":link,
+                "uid":data["uid"],
+                "userID":data["userID"]
+            }
+        # body='{"resultPlot":"0","uid":"8","userID":"2"}'
+        
+        print(bytes(json.dumps(body),'utf-8'))
+        producer.send('dataAnalysisConsumerF', bytes(json.dumps(body),'utf-8'))
         
             
 
