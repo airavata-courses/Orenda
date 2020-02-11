6be954d4591d870cd51b1b0c31886d52a0ebc458
milan <milanchheta@gmail.com>
2020-02-10 22:03:47 -0500

    data retrieval service kafka config #29

--- dataRetrieval/app.py
+++ dataRetrieval/app.py
@@ -1,21 +1,20 @@
 from flask import Flask
-import os
-from flask import Flask
-from flask_pymongo import PyMongo
-import urllib
-from kafka import KafkaConsumer
-consumer = KafkaConsumer('my_favorite_topic')
-for msg in consumer:
-  print (msg)
+from controllers import produce 
 
-from flask_kafka import FlaskKafka
-# create the flask object
+from pykafka import KafkaClient
+import json 
+
 app = Flask(__name__)
-# add mongo url to flask config, so that flask_pymongo can use it to make connection
-app.config['MONGO_URI'] = 'mongodb+srv://orendaUser:'+urllib.parse.quote_plus('Orenda@123')+'@cluster0-389i7.mongodb.net/test?retryWrites=true&w=majority/orendaDB'
-mongo = PyMongo(app)
+
+
+client = KafkaClient()
+consumer =  client.topics['dataRetrievalConsumer1'].get_simple_consumer()
+for message in consumer:
+    if message is not None:
+        data=json.loads((message.value))
+        print(data)
+        produce(data,client,message.value)
 
 
 
-if __name__ == "__main__":
-  app.run()
+
--- dataRetrieval/controllers.py
+++ dataRetrieval/controllers.py
@@ -1,14 +1,34 @@
-import matplotlib.pyplot as plt
-import pyart
-import nexradaws
 
-conn = nexradaws.NexradAwsInterface()
+from service import return_regions 
+# from models.dataRetrievalModel import dateRadar
+from pymongo import MongoClient
+from pykafka import KafkaClient
+import urllib
+import json
+cl = MongoClient("mongodb+srv://orendaUser:"+urllib.parse.quote_plus('Orenda@123')+"@cluster0-389i7.mongodb.net/test?retryWrites=true&w=majority")
+db = cl['dataRetrievalDB']
+col=db.dataEntries
 
-def return_regions(date_json_string):
-    month, day, year, radar = date_json_string.split("-")[0], date_json_string.split("-")[1], date_json_string.split("-")[2], date_json_string.split("-")[3]
-    radars = conn.get_avail_radars(str(year), str(month).zfill(2), str(day).zfill(2)) # year, month and day
-#     print(radars)
-    if (radar in radars):
-        print("success")
-    else: 
-        print("error")
+def produce(data,client,msg):
+    Month=data['data']['Month']
+    Day=data['data']['Day']
+    Year=data['data']['Year']
+    Radar=data['data']['Radar']
+    userID=data['data']['userID']
+    res=return_regions(Month,Day,Year,Radar)
+    print(res)
+    if(res=='success'):
+        body = {"Month":Month,"Day":Day,"Year":Year,"Radar":Radar,'userID':userID}
+        print(body)
+        r= col.insert_one(body)
+        with (client.topics['dataModellingConsumer1']).get_sync_producer() as producer:
+            producer.produce(msg)
+
+       
+    # else:
+    #     with (client.topics['apiGatewayConsumer']).get_sync_producer() as producer:
+    #         producer.produce(msg)
+      
+    else:
+        print('error')
+
