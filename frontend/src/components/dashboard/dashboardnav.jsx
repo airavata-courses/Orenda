a8b1da347229a38759b036b0d997098f410a1138
disha <dtalreja@iu.edu>
2020-02-11 19:04:02 -0500

    UI Dashboard #14

--- frontend/src/components/dashboard/dashboardnav.jsx
+++ frontend/src/components/dashboard/dashboardnav.jsx
@@ -1,29 +1,42 @@
-import React, { Component } from "react";
-import {Navbar,Container,Nav,Button} from 'react-bootstrap'
-import jwt_decode from 'jwt-decode'
+import React, { Component } from 'react';
+import { Navbar, Container, Nav, Button } from 'react-bootstrap';
+import jwt_decode from 'jwt-decode';
 
 export default class DashNav extends Component {
   // componentDidMount(){
-    // if (!localStorage.getItem('userInfo')){
-    //   this.props.history.push('/')
-    // }}
-  logOut(){
+  // if (!localStorage.getItem('userInfo')){
+  //   this.props.history.push('/')
+  // }}
+  logOut() {
     localStorage.removeItem('userInfo');
-this.props.history.push('/')
+    this.props.history.push('/');
   }
   render() {
-  
-    return ( 
-   <Navbar expand="lg" variant="light" bg="light" fixed="top">
-    <Navbar.Brand>Orenda</Navbar.Brand>
-    <Nav className="mr-auto">
-      <Nav.Link href={"/dashboard/submittask/"+ jwt_decode(localStorage.getItem("userInfo")).user.id} >Submit Task</Nav.Link>
-      <Nav.Link href={"/dashboard/session/"+ jwt_decode(localStorage.getItem("userInfo")).user.id} >Session</Nav.Link>
-      <Nav.Link onClick={this.logOut} href="/">Log Out</Nav.Link>
-
-      
-    </Nav>
-
-</Navbar>);
-}
+    return (
+      <Navbar expand='lg' bg='dark' variant='dark' fixed='top'>
+        <Navbar.Brand>Orenda</Navbar.Brand>
+        <Nav className='mr-auto'>
+          <Nav.Link
+            href={
+              '/dashboard/submittask/' +
+              jwt_decode(localStorage.getItem('userInfo')).user.id
+            }
+          >
+            Submit Task
+          </Nav.Link>
+          <Nav.Link
+            href={
+              '/dashboard/session/' +
+              jwt_decode(localStorage.getItem('userInfo')).user.id
+            }
+          >
+            Session
+          </Nav.Link>
+          <Nav.Link onClick={this.logOut} href='/'>
+            Log Out
+          </Nav.Link>
+        </Nav>
+      </Navbar>
+    );
+  }
 }
--- frontend/src/components/dashboard/submitTask.jsx
+++ frontend/src/components/dashboard/submitTask.jsx
@@ -1,72 +1,67 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import DashNav from "./dashboardnav";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import DashNav from './dashboardnav';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 import stations from './stations';
 import Select from 'react-select';
-import {Button} from 'react-bootstrap';
-import dashService from './dashboardservice'
-import jwt_decode from 'jwt-decode'
+import { Button } from 'react-bootstrap';
+import dashService from './dashboardservice';
+import jwt_decode from 'jwt-decode';
 
 export default class SubmitTask extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      userID:'',
-    errDate:'',
-      selectedDate:new Date(),
+      userID: '',
+      errDate: '',
+      selectedDate: new Date(),
       currYear: new Date().getFullYear(),
       currMonth: new Date().getMonth(),
       currDay: new Date().getDate(),
-stn:[],
-selectedStnVal:'',
-selectedStn:'',
-    Year: new Date().getFullYear(),
+      stn: [],
+      selectedStnVal: '',
+      selectedStn: '',
+      Year: new Date().getFullYear(),
       Month: new Date().getMonth(),
       Day: new Date().getDate()
-
-
     };
-    this.handleDateChange=this.handleDateChange.bind(this)
-    this.handleStnChange=this.handleStnChange.bind(this)
-
+    this.handleDateChange = this.handleDateChange.bind(this);
+    this.handleStnChange = this.handleStnChange.bind(this);
   }
-  componentDidMount(){
-if (localStorage.getItem('userInfo')){
-   
+  componentDidMount() {
+    if (localStorage.getItem('userInfo')) {
       this.setState({
         userID: jwt_decode(localStorage.getItem('userInfo')).user.id
       });
-   
-    
-    var statn=[]
-    var label=''
-    var value=''
-    var ob={}
-    for (var i = 0; i < stations.length; i++) {
-      label=stations[i]['STATION NAME']
-      value=stations[i]['STATION ID']
-       ob={label: label,value:value}
-       statn[i]=ob
+
+      var statn = [];
+      var label = '';
+      var value = '';
+      var ob = {};
+      for (var i = 0; i < stations.length; i++) {
+        label = stations[i]['STATION NAME'];
+        value = stations[i]['STATION ID'];
+        ob = { label: label, value: value };
+        statn[i] = ob;
+      }
+      this.setState({
+        stn: statn
+      });
+    } else {
+      this.props.history.push('/');
     }
-    this.setState({
-      stn: statn
-    });
   }
-else{
-  this.props.history.push('/')
-}}
-  handleStnChange = stn => { console.log(stn) 
-    this.setState({selectedStn:stn,selectedStnVal:stn['value']})}
+  handleStnChange = stn => {
+    console.log(stn);
+    this.setState({ selectedStn: stn, selectedStnVal: stn['value'] });
+  };
 
   handleDateChange = date => {
-    
-   
     if (
       date.getFullYear() <= this.state.currYear &&
       date.getMonth() <= this.state.currMonth &&
-      date.getDate() <=this.state.currDay
+      date.getDate() <= this.state.currDay
     ) {
       var year = date.getFullYear();
       var month = date.getMonth();
@@ -75,63 +70,73 @@ else{
         Year: year,
         Month: month,
         Day: day,
-        selectedDate:date,
-        errDate: ""
+        selectedDate: date,
+        errDate: ''
       });
     } else {
       this.setState({
-        errDate: "Please select a valid date"
+        errDate: 'Please select a valid date'
       });
     }
-    
   };
-submitTask=e=>{
-e.preventDefault();
+  submitTask = e => {
+    e.preventDefault();
 
-var data = {
-  Year: this.state.Year,
-  Month: this.state.Month,
-  Day: this.state.Day,
-  Radar: this.state.selectedStnVal,
-  userID:this.state.userID
-};
-console.log(data)
-dashService.submitTask(data).then(res => {
-  if (res.status === 200) {
-    console.log(res)
-  }
-});
-}
+    var data = {
+      Year: this.state.Year,
+      Month: this.state.Month,
+      Day: this.state.Day,
+      Radar: this.state.selectedStnVal,
+      userID: this.state.userID
+    };
+    console.log(data);
+    dashService.submitTask(data).then(res => {
+      if (res.status === 200) {
+        console.log(res);
+      }
+    });
+  };
   render() {
-
     return (
-      <div className="mt-5 p-4 ">
+      <div className='mt-5 p-4 '>
         <DashNav />
         {this.state.errDate}
-        <div className="container m-2">
-          <div className="row">
-            <div className="col">
-              <DatePicker
-                selected={this.state.selectedDate}
-                onChange={this.handleDateChange}
-              />
+        <center>
+          <div className='container m-2'>
+            <div className='row'>
+              <div className='col'>
+                <b>
+                  <font size='8'> GET THE PREDICTED WEATHER PLOT</font>
+                </b>
+              </div>
             </div>
-          </div>{" "}
-       
-          <div className="row ">
-            <div className="col ">
-              <Select
-                name="stations"
-           
-                options={this.state.stn}
-                onChange={val => this.handleStnChange(val)}
-                value={this.state.selectedStn}
-              />
-            </div>
-          </div>{" "}
-        </div>{" "}
-
-        <Button variant="primary" onClick={this.submitTask}>Validate And Submit</Button>
+            <div className='row mt-5'>
+              <div className='col'>
+                <b>Select a date</b>
+                <DatePicker
+                  selected={this.state.selectedDate}
+                  onChange={this.handleDateChange}
+                />
+              </div>
+            </div>{' '}
+            <div className='row mt-5 '>
+              <div className='col '>
+                <b>Select a location</b>
+                <Select
+                  data-size='5'
+                  data-width='fit'
+                  name='stations'
+                  options={this.state.stn}
+                  onChange={val => this.handleStnChange(val)}
+                  value={this.state.selectedStn}
+                />
+              </div>
+            </div>{' '}
+          </div>{' '}
+          <Button variant='primary' onClick={this.submitTask}>
+            Validate And Submit
+          </Button>
+        </center>
       </div>
     );
   }
