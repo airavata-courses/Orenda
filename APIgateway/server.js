dd6fb1bf7fa8f4360b08bb2ad8edaa9809eed3ef
milan <milanchheta@gmail.com>
2020-02-12 00:44:41 -0500

    kafka config

--- APIgateway/server.js
+++ APIgateway/server.js
@@ -3,7 +3,7 @@ const app = express();
 const dotenv = require("dotenv");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const consumer=require('./config/kafkaConfig').consumer
+
 const router = express.Router();
 var kafka = require('kafka-node')
 global.resID={}
@@ -16,34 +16,36 @@ app.use(bodyParser.json());
 app.use(cors());
 
 require("./routing/routes")(app);
-// var topicsToCreate = [{
-//   topic: 'apiGatewayConsumerF',
-//   partitions: 1,
-//   replicationFactor: 1
-// },{
-//   topic: 'dataRetrievalConsumerF',
-//   partitions: 1,
-//   replicationFactor: 1
-// },{
-//   topic: 'dataModellingConsumerF',
-//   partitions: 1,
-//   replicationFactor: 1
-// },{
-//   topic: 'dataAnalysisConsumerF',
-//   partitions: 1,
-//   replicationFactor: 1
-// },{
-//   topic: 'sessionManagementConsumerF',
-//   partitions: 1,
-//   replicationFactor: 1
-// },{
-//   topic: 'sessionManagementConsumerApiF',
-//   partitions: 1,
-//   replicationFactor: 1
-// }]
-// client.createTopics(topicsToCreate, (error, result) => {
-//   console.log(result)
-// });
+var topicsToCreate = [{
+  topic: 'apiGatewayConsumerF',
+  partitions: 1,
+  replicationFactor: 1
+},{
+  topic: 'dataRetrievalConsumerF',
+  partitions: 1,
+  replicationFactor: 1
+},{
+  topic: 'dataModellingConsumerF',
+  partitions: 1,
+  replicationFactor: 1
+},{
+  topic: 'dataAnalysisConsumerF',
+  partitions: 1,
+  replicationFactor: 1
+},{
+  topic: 'sessionManagementConsumerF',
+  partitions: 1,
+  replicationFactor: 1
+},{
+  topic: 'sessionManagementConsumerApiF',
+  partitions: 1,
+  replicationFactor: 1
+}]
+client.createTopics(topicsToCreate, (error, result) => {
+  console.log(result)
+});
+const consumer=require('./config/kafkaConfig').consumer
+
 app.listen(process.env.PORT, () => {
   console.log("gateway listening on port " + process.env.PORT);
 });
