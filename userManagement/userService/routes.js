1f8bd3ab5f76a2dcfc2f74aa1e5fa14e091b9e73
milan <milanchheta@gmail.com>
2020-02-24 02:13:44 -0500

    new branch created for dockerizing api gateway

--- userManagement/userService/routes.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(app) {
-  let user = require("./controllers/user_controller");
-  // Login routes
-  app.post("/login", user.login);
-  app.post("/register", user.register);
-//   app.post("/reset_password", user.reset);
-};
