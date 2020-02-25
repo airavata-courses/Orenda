1e75919a7df3845e02a20af8c9dafd58037cdb50
milan <milanchheta@gmail.com>
2020-02-25 02:06:03 -0500

    api gateway : code cleaning and dockerizing #69 #70

--- APIgateway/routing/routes.js
+++ APIgateway/routing/routes.js
@@ -1,33 +1,10 @@
 
 
 module.exports = function(app) {
-        
-        let task = require("./eventHandler");
-        const uuidv1 = require('uuid/v1');
+        let config = require('../config/config');
+        let eventHandler = require("./eventHandler");
 
-        app.post("/session",function( req,res){
-
-                let uid=uuidv1()
-                msg=req.body
-                id=msg['userID']
-               
-                resID[uid]=res
-                let data={"uid":uid,'userID':id}
-
-                task.produce(data,'sessionManagementConsumerApiF')
-        });
-
-        app.post("/task",function( req,res){
-                
-                let uid=uuidv1()
-                msg=req.body
-                id=msg['userID']
-                let data={"inputData":msg['inputData'],"uid":uid,'userID':id}
-               
-                // resID[uid]=res
-                
-                task.produce(data,'dataRetrievalConsumerF')
-
-        });
+        app.post(config.routes.session,eventHandler.session);
+        app.post(config.routes.task,eventHandler.task);
 
 };
