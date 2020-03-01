c67da7252cf3a40631a7b55e109f466cfb227f4f
milan <milanchheta@gmail.com>
2020-03-01 01:27:07 -0500

    updated

---
+++ frontend/src/components/dashboard/dashboardservice.jsx
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const USER_API_BASE_URL = 'http://localhost:5000/';
+// const USER_API_BASE_URL = '0.0.0.0:5000/';
+
+class dashService {
+
+    submitTask(data){
+        return axios.post(USER_API_BASE_URL + "task", data);
+    }
+    sessions(data){
+        return axios.post(USER_API_BASE_URL + "session", data);
+    }
+
+    
+}
+
+export default new dashService();
