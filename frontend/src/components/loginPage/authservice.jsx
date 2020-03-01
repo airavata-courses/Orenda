81e4fce6388218734a7c81e0875ec090b80fb0dc
milan <milanchheta@gmail.com>
2020-03-01 01:26:16 -0500

    updated

---
+++ frontend/src/components/loginPage/authservice.jsx
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+// const USER_API_BASE_URL = '0.0.0.0:5009/';
+const USER_API_BASE_URL = 'http://localhost:5009/';
+
+class AuthService {
+
+    login(credentials){
+        return axios.post(USER_API_BASE_URL + "login", credentials);
+    }
+
+    register(userData){
+        return axios.post(USER_API_BASE_URL + "register", userData);
+    }
+
+    getUserInfo(){
+        return JSON.parse(localStorage.getItem("userInfo"));
+    }
+
+    getAuthHeader() {
+       return {headers: {Authorization: 'Bearer ' + this.getUserInfo().token }};
+    }
+
+    logOut() {
+        localStorage.removeItem("userInfo");
+        return axios.post(USER_API_BASE_URL + 'logout', {}, this.getAuthHeader());
+    }
+}
+
+export default new AuthService();
