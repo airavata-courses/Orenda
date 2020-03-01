e2e12472b932729198d3810aa1d5b640271d2502
milan <milanchheta@gmail.com>
2020-03-01 01:14:51 -0500

    updated

---
+++ sessionManagement/sessionService/models/session_model.js
@@ -0,0 +1,27 @@
+let mongoose = require('mongoose');
+
+
+var sessionSchema = new mongoose.Schema({
+  uid: {
+    type: String,
+    required: true
+  },
+  userID: {
+    type: String,
+    required: true
+   
+  },
+  inputData: {
+    type: JSON,
+    required: true
+  },
+  outputData: {
+    type: String
+  },
+  taskState:{
+    type: String,
+    default:'None'
+  }
+});
+sessionSchema.set('timestamps', true);
+module.exports = mongoose.model('sessiondetails', sessionSchema);
