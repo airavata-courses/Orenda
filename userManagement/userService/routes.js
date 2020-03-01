4b50e0a5c165e8822dc141afc108621fda48a452
milan <milanchheta@gmail.com>
2020-03-01 01:12:28 -0500

    updated

---
+++ userManagement/userService/routes.js
@@ -0,0 +1,9 @@
+module.exports = function(app) {
+  let user = require("./controllers/user_controller");
+  let config = require("../config/config");
+
+  // Login routes
+  app.post(config.routes.login, user.login);
+  app.post(config.routes.register, user.register);
+//   app.post("/reset_password", user.reset);
+};
