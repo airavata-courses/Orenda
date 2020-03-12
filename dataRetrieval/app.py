896e60458559dfafccf7dab694c6f1e16e3d407b
milan <milanchheta@gmail.com>
2020-03-12 15:09:50 -0400

    minor fixes

--- dataRetrieval/app.py
+++ dataRetrieval/app.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from controllers import produce 
-
 from pykafka import KafkaClient
 import json 
 
@@ -8,11 +7,12 @@ app = Flask(__name__)
 
 
 client = KafkaClient(hosts="kafka-service:9092")
-consumer =  client.topics['dataRetrievalConsumerF'].get_simple_consumer(consumer_group="dataModellingConsumerF",
+consumer =  client.topics['dataRetrievalConsumerF'].get_simple_consumer(consumer_group="test-consumer-group",
                                      auto_commit_enable=True)
 
 for message in consumer:
     if message is not None:
+        print("task received at dataRetrievalConsumerF")
         data=json.loads((message.value))
         produce(data,client)
         
