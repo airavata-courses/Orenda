04b127bf1ff69996ec9d96560ca7ba5619b10ffa
milan <milanchheta@gmail.com>
2020-03-12 19:22:47 -0400

    deployment files for retieval and modelling

--- data_analysis/kafkaDataAnalysis/src/main/java/com/example/data_analysis/DemoApplication.java
+++ data_analysis/kafkaDataAnalysis/src/main/java/com/example/data_analysis/DemoApplication.java
@@ -4,14 +4,11 @@ import com.example.data_analysis.engine.Consumer;
 import com.example.data_analysis.engine.Producer;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;import org.springframework.context.annotation.Bean;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
-
-	
-
-
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
