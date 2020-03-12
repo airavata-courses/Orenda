b1ed54c73dadfde725e4fcfbe07e3474e7ec702b
milan <milanchheta@gmail.com>
2020-03-12 17:49:03 -0400

    minor chnages

--- APIgateway/routing/eventHandler.js
+++ APIgateway/routing/eventHandler.js
@@ -3,14 +3,11 @@ const producer = require("../config/kafkaConfig").producer;
 const uuidv1 = require("uuid/v1");
 let config = require("../config/config");
 
-<<<<<<< HEAD
-=======
 async function serverStarted(req, res) {
 return res.send("Server Started")
 
 }
 
->>>>>>> develop
 async function session(req, res) {
 
   let uid = uuidv1();
@@ -40,11 +37,7 @@ async function task(req, res) {
   );
   if (result != "error") {
     res.sendStatus(200);
-<<<<<<< HEAD
-    console.log('task submitted')
-=======
     console.log('task submitted to' +config.kafkaTopics.producers.dataRetrieval)
->>>>>>> develop
   } else {
     res.sendStatus(400);
   }
@@ -69,8 +62,4 @@ async function produce(msg, topic) {
   });
 }
 
-<<<<<<< HEAD
-module.exports = { session, task };
-=======
 module.exports = { session, task ,serverStarted};
->>>>>>> develop
--- APIgateway/routing/routes.js
+++ APIgateway/routing/routes.js
@@ -6,9 +6,6 @@ module.exports = function(app) {
 
         app.post(config.routes.session,eventHandler.session);
         app.post(config.routes.task,eventHandler.task);
-<<<<<<< HEAD
-=======
         app.get("/",eventHandler.serverStarted);
->>>>>>> develop
 
 };
