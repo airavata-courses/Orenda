db2801729064a49594458dacb48d8639cd020810
milan <milanchheta@gmail.com>
2020-03-13 20:24:06 -0400

    #145

--- frontend/src/components/dashboard/dashboardservice.jsx
+++ frontend/src/components/dashboard/dashboardservice.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const USER_API_BASE_URL = 'http://localhost:5000/';
+const USER_API_BASE_URL = 'http://apigatewayservice:5000/';
 // const USER_API_BASE_URL = '0.0.0.0:5000/';
 
 class dashService {
