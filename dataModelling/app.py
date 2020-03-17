0409642087effbf6ff56d5c9c917f36474ef6e6f
milan <milanchheta@gmail.com>
2020-03-17 01:52:11 -0400

    kafka error resolved #147

--- dataModelling/app.py
+++ dataModelling/app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 client = KafkaClient(hosts="kafka-service:9092")
 consumer =  client.topics['dataModellingConsumerF'].get_balanced_consumer(consumer_group="data-modelling",
-                                     auto_commit_enable=True)
+                                     auto_commit_enable=True,zookeeper_connect="zoo1:2181")
 
 for message in consumer:
     print("task received at dataModellingConsumerF")
--- dataRetrieval/app.py
+++ dataRetrieval/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 client = KafkaClient(hosts="kafka-service:9092")
 consumer =  client.topics['dataRetrievalConsumerF'].get_balanced_consumer(consumer_group="data-retrieval",
-                                     auto_commit_enable=True)
+                                     auto_commit_enable=True,zookeeper_connect="zoo1:2181")
 
 for message in consumer:
     if message is not None:
