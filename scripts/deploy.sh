b2a05dae3845656af97e87d89d5231a615072146
milan <milanchheta@gmail.com>
2020-03-20 19:59:38 -0400

    automation setup #160

---
+++ scripts/deploy.sh
@@ -0,0 +1,13 @@
+/bin/bash ./deployApi.sh
+/bin/bash ./deployModel.sh
+/bin/bash ./deployAnalysis.sh
+/bin/bash ./deployRetrieve.sh
+/bin/bash ./deploySession.sh
+/bin/bash ./deployFrontend.sh
+/bin/bash ./deployUser.sh
+
+
+
+
+
+
