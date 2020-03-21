eb22938f03d73bcbdbb02837517f4705124af072
milanchheta <30660458+milanchheta@users.noreply.github.com>
2020-03-21 00:50:34 -0400

    Updated arguments

--- deployFiles/deploy.sh
+++ deployFiles/deploy.sh
@@ -1,10 +1,10 @@
-/bin/bash ./deployApi.sh
-/bin/bash ./deployModel.sh
-/bin/bash ./deployAnalysis.sh
-/bin/bash ./deployRetrieve.sh
-/bin/bash ./deploySession.sh
-/bin/bash ./deployFrontend.sh
-/bin/bash ./deployUser.sh
+/bin/bash ./deployApi.sh 88dbeb83ab26ee8c40e2a67d5d1f56d77ce1827c
+/bin/bash ./deployModel.sh c7d445dbc31af7d1eae53f9f57997acd17de31f2
+/bin/bash ./deployAnalysis.sh bc65dec6b31c39303aa17bda39dcb091a8d0ecdf
+/bin/bash ./deployRetrieve.sh d10ef75851ac0d59edfda56b34dc3482d5cb1fc2
+/bin/bash ./deploySession.sh 35be8e6d50720a961b9829c10235a17903cd62a6
+/bin/bash ./deployFrontend.sh b0cb01411ba425f247fe74b7607bf20daf8ef8e5
+/bin/bash ./deployUser.sh 369d9b8dc16c52bbacff7dbe495fef0a8932dd5e
 
 
 
